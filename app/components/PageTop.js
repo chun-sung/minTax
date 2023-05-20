a070d1f7e64807e35bf26931169482d8b329e071
Kim Chun-Sung <springstar@daum.net>
2023-05-21 01:05:26 +0900

    cover 삭제

--- app/components/PageTop.js
+++ app/components/PageTop.js
@@ -9,17 +9,17 @@ export default function PageTop() {
         window.scrollTo(0,0)  // 최상단 이동                        
         setFade('end2')        
 
-        setTimeout(()=> {
-            document.querySelector('.any')?.classList.add('none') // 커버 삭제 display: 'none'
-        }, 500)
+        // setTimeout(()=> {
+        //     document.querySelector('.any')?.classList.add('none') // 커버 삭제 display: 'none'
+        // }, 300)
         return () => setFade('')  // 애니메이션 초기화 (클린업펑션)
     },[])
   
     useEffect(()=>{
         setTimeout(()=> {
             document.querySelector('.any').classList.add('none') // 커버 삭제 display: 'none'
-        }, 500)
-    },[])
+        }, 300)
+    })
 
     return (<>
             {/* 처음 쿠키 값은 lightMode 에 셋팅 된다. 그에 맞춰 hydration 에러를 처음에 피하기 위해.....  어쨌거나 처음에러만 피하자 이후 미스 매치는 어쩔수 없음*/}
